7ea99af7e70b1f3cd94f2068ec291e5603c6f795
Kristof <kristof@aut.bme.hu>
2013-05-24 10:42:52 +0200

    PhoneServer can now register the SMEyeL node on a web based registry site.

--- libCommunication/include/PhoneServer.h
+++ libCommunication/include/PhoneServer.h
@@ -18,6 +18,8 @@ using namespace std;
 class PhoneServer : public PhoneProxy
 {
 private:
+	int InitSocket();
+	void CloseSocket(SOCKET socket);
 
 public:
 	SOCKET serversock;	// public only for debug...
@@ -27,10 +29,20 @@ public:
 		lastReceivedTimeStamp=0;
 	}
 
-	void InitServer(int port);
+	int InitServer(int port);
 	void ListenServerSocket();
 	void DisconnectServer();
 
+	/** Registers the server on a SMEyeL node registry server.
+		(Do not have to call InitServer before this. This only registers the servers presence.)
+		The registrationURL is sent to the server defined by registryHost on port 80 as a HTTP GET request.
+
+		@param	registryHost	Address (IP or URL) of the server, like "avalon.aut.bme.hu".
+		@param	registrationURL	Path on the server for the registration script, like
+								"~kristof/smeyel/smeyel_reg.php?IP=127.0.0.1:6000". (Without starting "/".)
+		@return				0 for success, <0 for error codes.
+	*/
+	int RegisterNode(const char *registryHost, const char *registrationURL);
 };
 
 #endif
--- libCommunication/src/PhoneServer.cpp
+++ libCommunication/src/PhoneServer.cpp
@@ -3,31 +3,23 @@
 
 using namespace LogConfigTime;
 
-void PhoneServer::InitServer(int port)
+int PhoneServer::InitServer(int port)
 {
 	struct sockaddr_in server;
     struct hostent *host_info;
     unsigned long addr;
 	int iResult;
 
-#ifdef WIN32
-    WORD wVersionRequested;
-    WSADATA wsaData;
-    wVersionRequested = MAKEWORD (1, 1);
-    if (WSAStartup (wVersionRequested, &wsaData) != 0)
+	if (InitSocket())
 	{
-		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"CamClient","Error: Cannot initialize winsock!\n");
-		exit(1);
+		return -1;
 	}
-#else
-#error TODO: Socket init not implemented for this platform!
-#endif
 
     serversock = socket( AF_INET, SOCK_STREAM, 0 );
     if (serversock < 0)
 	{
-		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"CamClient","Error: Cannot create server socket!\n");
-		exit(2);
+		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot create server socket!\n");
+		return -2;
 	}
 
     memset( &server, 0, sizeof (server));
@@ -38,27 +30,123 @@ void PhoneServer::InitServer(int port)
 	// Binding the socket
 	if (bind(serversock, (struct sockaddr *) &server, sizeof(server)) < 0)
 	{
-		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"CamClient","Error: Cannot bind server socket!\n");
-		exit(3);
+		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot bind server socket!\n");
+		return -3;
 	}
-	Logger::getInstance()->Log(Logger::LOGLEVEL_INFO,"CamClient","Server socket initialized on port %d\n",port);
+	Logger::getInstance()->Log(Logger::LOGLEVEL_INFO,"PhoneServer","Server socket initialized on port %d\n",port);
+	return 0;
 }
 
 void PhoneServer::ListenServerSocket()
 {
 	listen(serversock,5);
-	Logger::getInstance()->Log(Logger::LOGLEVEL_INFO,"CamClient","Server socket now listening.\n");
+	Logger::getInstance()->Log(Logger::LOGLEVEL_INFO,"PhoneServer","Server socket now listening.\n");
 }
 
 void PhoneServer::DisconnectServer()
 {
 	shutdown(serversock, SD_SEND);
+	CloseSocket(serversock);
+	serversock = -1;
+}
+
+// Call after Init so that port is already set...
+// Connects
+int PhoneServer::RegisterNode(const char *registryHost, const char *registrationURL)
+{
+	struct sockaddr_in server;
+    struct hostent *host_info;
+    unsigned long addr;
+	int iResult;
+
+	if (InitSocket())
+	{
+		return -1;
+	}
+
+    SOCKET regSocket = socket( AF_INET, SOCK_STREAM, 0 );
+
+    if (regSocket < 0)
+	{
+		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot register the node, cannot create socket!\n");
+		CloseSocket(regSocket);
+		return -2;
+	}
+
+    memset( &server, 0, sizeof (server));
+    if ((addr = inet_addr(registryHost)) != INADDR_NONE) {
+        memcpy( (char *)&server.sin_addr, &addr, sizeof(addr));
+    }
+    else {
+        host_info = gethostbyname(registryHost);
+        if (NULL == host_info)
+		{
+			Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot register the node, unknown server!\n");
+			CloseSocket(regSocket);
+			return -3;
+		}
+        memcpy( (char *)&server.sin_addr,
+                host_info->h_addr, host_info->h_length );
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+
+    if(connect(regSocket,(struct sockaddr*)&server,sizeof(server)) <0)
+	{
+		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot register the node, connect failed!\n");		
+		CloseSocket(regSocket);
+		return -4;
+	}
+
+	// Send registration HTTP GET
+	char buffer[256];
+	memset(buffer,0,256);
+
+	sprintf(buffer,"GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: HTMLGET 1.0\r\n\r\n",registrationURL,registryHost);
+	int len=strlen(buffer);
+
+	int sent = 0;
+	while(sent < strlen(buffer))
+	{ 
+		int tmpres = send(regSocket, buffer+sent, len-sent, 0);
+		if(tmpres == -1){
+			Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot register the node, send failed!\n");		
+			CloseSocket(regSocket);
+			return -5;
+		}
+		sent += tmpres;
+	}
+
+	// Close connection
+	shutdown(regSocket, SD_SEND);
+	CloseSocket(regSocket);
+	return 0;
+}
+
+int PhoneServer::InitSocket()
+{
 #ifdef WIN32
-	closesocket(serversock);
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    wVersionRequested = MAKEWORD (1, 1);
+    if (WSAStartup (wVersionRequested, &wsaData) != 0)
+	{
+		Logger::getInstance()->Log(Logger::LOGLEVEL_ERROR,"PhoneServer","Error: Cannot initialize winsock!\n");
+		return -1;
+	}
+#else
+#error TODO: Socket init not implemented for this platform!
+#endif
+	return 0;
+}
+
+void PhoneServer::CloseSocket(SOCKET socketToClose)
+{
+#ifdef WIN32
+	closesocket(socketToClose);
 	WSACleanup();
 #else
 #error TODO: Socket disconnect is not implemented for this platform.
 #endif
-
-	serversock = -1;
 }
