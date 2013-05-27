d32a22547aae9b2ca9a44f0bffa19bf3d67f992a
KOSZO Norbert <k.norbi@gmail.com>
2013-05-27 14:51:20 +0200

    excluding ps3eye parts from linux build, and fixing switch-break problem

--- libVideoInput/src/VideoInputFactory.cpp
+++ libVideoInput/src/VideoInputFactory.cpp
@@ -1,7 +1,9 @@
 #include "VideoInputFactory.h"
 
 #include "VideoInputGeneric.h"
+#ifdef _WIN32
 #include "VideoInputPs3Eye.h"
+#endif
 
 #define VIDEOINPUTTYPE_GENERIC	0
 #define VIDEOINPUTTYPE_PS3EYE	1
@@ -12,8 +14,12 @@ VideoInput *VideoInputFactory::CreateVideoInput(int videoInputType)
 	{
 	case VIDEOINPUTTYPE_GENERIC:
 		return new VideoInputGeneric();
+	break;
+#ifdef _WIN32
 	case VIDEOINPUTTYPE_PS3EYE:
 		return new VideoInputPs3Eye();
+	break;
+#endif
 	}
 	return NULL;
 }
