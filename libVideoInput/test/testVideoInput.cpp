317ae6024e1b1fef0cb87e285d8226be4a9c21db
KOSZO Norbert <k.norbi@gmail.com>
2013-08-02 13:18:38 +0200

    Simple test application for libVideoInput

---
+++ libVideoInput/test/testVideoInput.cpp
@@ -0,0 +1,19 @@
+#include "VideoInputFactory.h"
+#include <opencv2/core/core.hpp>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    cv::Mat frame(480,640,CV_8UC3);
+    VideoInput *vi = VideoInputFactory::CreateVideoInput(VIDEOINPUTTYPE_V4L2, 1);
+
+
+    vi->SetNormalizedExposure(0);
+    for (int i = 0; i < 10; ++i) vi->captureFrame(frame);
+    cv::imwrite("low_exp.jpg", frame);
+    vi->SetNormalizedExposure(90);
+    for (int i = 0; i < 10; ++i) vi->captureFrame(frame);
+    cv::imwrite("high_exp.jpg", frame);
+    delete vi;
+}
