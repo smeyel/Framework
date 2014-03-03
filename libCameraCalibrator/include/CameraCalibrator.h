99a83319d425c91e4af778566509927457b71a1d
Gabor Toth <tgabor.flyr@gmail.com>
2014-03-03 12:12:03 +0100

    Camera calibrator to calculate intrinsic params
    Camera calibrator calculates intrinsic and distortion coeffs parameters
    for camera, and exports to XML.

---
+++ libCameraCalibrator/include/CameraCalibrator.h
@@ -0,0 +1,84 @@
+#ifndef __CAMERACALIBRATOR_H
+#define __CAMERACALIBRATOR_H
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <time.h>
+#include <stdio.h>
+
+using namespace cv;
+
+/** Calculate intrinsic and distortion coeffs parameters of camera
+
+	Usage: (for example, with CameraLocalProxy)
+	
+	1. Get instantiate of CameraLocalProxy and CameraCalibrator
+	CameraLocalProxy camProxy0 = CameraLocalProxy(VIDEOINPUTTYPE_GENERIC, 0);
+	CameraCalibrator cameraCalibrator = CameraCalibrator();
+
+	2. Capture image continuously
+		- capture image
+		- calibrate this image
+		- if calibration finished, save camera params
+
+	while(1){
+		waitKey(50);
+		camProxy0.CaptureImage(0, image);
+		int retval = cameraCalibrator.calibrate(image, true);
+		if(retval == cameraCalibrator.FINISHED)
+		{
+			cameraCalibrator.saveCameraParams("intrinsic.xml");
+			break;
+		}
+		
+		imshow("Image View", *image);
+	}
+*/
+class CameraCalibrator 
+{
+private:
+	Size boardSize;
+	Size imageSize;
+    float squareSize;
+	float aspectRatio;
+    Mat* cameraMatrix;
+	Mat* distCoeffs;
+	
+    int nframes;	// number of chessboard testing
+	int flags;
+    int delay;
+	clock_t prevTimestamp;
+    vector<vector<Point2f>> imagePoints;
+
+public:
+	enum { FINISHED = 1, NOT_FINISHED = 0, ERROR = -1 };
+	/** Constructor */
+	CameraCalibrator();
+	/** Constructor */
+	CameraCalibrator(Size _boardSize, int _nframes);
+	
+	/** Common init function for Constructors */
+	void initDefaults(Size boardSize = Size(9,6), int nframes = 10);
+	/** Find and draw chessboard corners, and store imagePoints for calibration */
+	int calibrate(Mat* target, bool showResultOnImage = false);
+	/** Calibration and calculate intrinsic and distortion coeffs parameters */
+	bool runCalibration();
+	/** Calculate corners of Chessboard */
+	void calcChessboardCorners(vector<Point3f>& corners);
+	
+	/** Save intrinsic and distortion coeffs parameters to file */
+	void saveCameraParams(const string& filename);
+	/** Distort the input frame */
+	void showImageAsDistorted(Mat* target);
+	/** Get camera matrix (intrinsic matrix) */
+	Mat* getCameraMatrix();
+	/** Get distortion coeffs matrix */
+	Mat* getDistortionCoefficients();
+
+	~CameraCalibrator();
+};
+
+#endif
---
+++ libCameraCalibrator/src/CameraCalibrator.cpp
@@ -0,0 +1,125 @@
+#include "CameraCalibrator.h"
+
+CameraCalibrator::CameraCalibrator()
+{
+	CameraCalibrator::initDefaults();
+}
+
+CameraCalibrator::CameraCalibrator(Size _boardSize, int _nframes)
+{
+	CameraCalibrator::initDefaults(_boardSize, _nframes);
+}
+
+CameraCalibrator::~CameraCalibrator()
+{
+	if(cameraMatrix)
+		delete cameraMatrix;
+	cameraMatrix = NULL;
+
+	if(distCoeffs)
+		delete distCoeffs;
+	distCoeffs = NULL;
+}
+
+void CameraCalibrator::initDefaults(Size _boardSize, int _nframes)
+{
+	cameraMatrix = new Mat(3, 3, CV_64F);
+	distCoeffs = new Mat(8, 1, CV_64F);
+	boardSize = _boardSize;
+	nframes = _nframes;
+	prevTimestamp = 0;
+	delay = 1000;
+	flags = 0;
+	squareSize = 1.f;
+	aspectRatio = 1.f;
+	imagePoints.clear();
+}
+
+int CameraCalibrator::calibrate(Mat* target, bool showResultOnImage)
+{
+	Mat viewGray;
+	vector<Point2f> pointbuf;
+
+	imageSize = target->size();
+	cvtColor(*target, viewGray, COLOR_BGR2GRAY);
+	bool found = findChessboardCorners(*target, boardSize, pointbuf, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE);
+
+	if(found)
+	{
+		cornerSubPix(viewGray, pointbuf, Size(11,11), Size(-1,-1), TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 30, 0.1 ));
+			
+		if(showResultOnImage)
+			drawChessboardCorners(*target, boardSize, Mat(pointbuf), found);
+
+		if(clock() - prevTimestamp > delay*1e-3*CLOCKS_PER_SEC)
+		{
+			imagePoints.push_back(pointbuf);
+			prevTimestamp = clock();
+		}
+	}
+
+	if(imagePoints.size() >= (unsigned)nframes )
+	{
+		return runCalibration() ? FINISHED : ERROR;
+	}
+
+	return NOT_FINISHED;
+}
+
+bool CameraCalibrator::runCalibration()
+{
+	cameraMatrix->eye(3, 3, CV_64F);
+	if( flags & CV_CALIB_FIX_ASPECT_RATIO ){
+		cameraMatrix->at<double>(0,0) = aspectRatio;
+	}
+
+	distCoeffs->zeros(8, 1, CV_64F);
+
+	vector<vector<Point3f>> objectPoints(1);
+	calcChessboardCorners(objectPoints[0]);
+
+	objectPoints.resize(imagePoints.size(),objectPoints[0]);
+	
+	vector<Mat> rvecs, tvecs;
+	calibrateCamera(objectPoints, imagePoints, imageSize, *cameraMatrix, *distCoeffs, rvecs, tvecs, flags|CV_CALIB_FIX_K4|CV_CALIB_FIX_K5);
+
+	return (checkRange(*cameraMatrix) && checkRange(*distCoeffs));
+}
+
+void CameraCalibrator::calcChessboardCorners(vector<Point3f>& corners)
+{
+	corners.resize(0);
+
+	for( int i = 0; i < boardSize.height; i++ )
+		for( int j = 0; j < boardSize.width; j++ )
+			corners.push_back(Point3f(float(j*squareSize), float(i*squareSize), 0));
+}
+
+void CameraCalibrator::saveCameraParams(const string& filename)
+{
+	FileStorage fs( filename, FileStorage::WRITE );
+	fs << "Camera_Matrix" << *cameraMatrix;
+	fs << "Distortion_Coefficients" << *distCoeffs;
+}
+
+void CameraCalibrator::showImageAsDistorted(Mat* target)
+{
+	Mat temp = target->clone();
+	undistort(temp, *target, *cameraMatrix, *distCoeffs);
+}
+
+Mat* CameraCalibrator::getCameraMatrix()
+{
+	if(cameraMatrix)
+		return cameraMatrix;
+	else
+		return NULL;
+}
+
+Mat* CameraCalibrator::getDistortionCoefficients()
+{
+	if(distCoeffs)
+		return distCoeffs;
+	else
+		return NULL;
+}
